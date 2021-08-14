d59afe734d5733bbac4a1bab7aea8d76b3b6a84f
vivek-sundli <33592335+vivek-sundli@users.noreply.github.com>
2021-08-14 21:12:20 +0530

    Update eNearestKthPoint.cpp

--- eNearestKthPoint.cpp
+++ eNearestKthPoint.cpp
@@ -1,27 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+using namespace std;
 
-void displayKthDistance(priority_queue pq,int kth){
-	cout<<pq[kth];
-}
+#define int long long 
 
-long long rocketDistance(x[i],y[i]){
-	return x*x +y*y;
-}
-
-int main() {
-	int t,k,q;
-	cin>>t>>k;
-	priority_queue<long long,classCompare> pq;
-	while(t--){
-		cin>>q;
-		if(q==1){
-			vector<int> x;
-			vector<int> y;
-			cin>>x>>y;
-			pq.insert(rocketDistancex([i],y[i]));
-		}else{
-			displayKthDistance(pq,k);
+int32_t main () {
+	int n, k; cin >> n >> k;
+	priority_queue <int> pq;
+	for (int i = 0; i < n; i++) {
+		int qtype; cin >> qtype;
+		if (qtype == 1) {
+			int x, y; cin >> x >> y;
+			pq.push(x*x + y*y);
+		} else {
+			while (pq.size() != k) {
+				pq.pop();
+			}
+			cout << pq.top() << "\n";
 		}
-	}	
+	}
 	return 0;
 }
