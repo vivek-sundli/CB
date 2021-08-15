ad84f232d53835cdc96a6ff453359b8a1c21e6f8
vivek-sundli <33592335+vivek-sundli@users.noreply.github.com>
2021-08-15 23:42:51 +0530

    Create eStringSub.cpp

---
+++ eStringSub.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string s1,s2;
+	cin>>s1>>s2;
+	int i,j,w1,w2;
+	for(i=0;i<s1.length;i++){
+		for(j=0;j<s2.length;j++){
+			if(s2[j]==s1[i]){
+				w1=
+			}
+		}
+	}
+	
+	return 0;
+}
